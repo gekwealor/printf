c617a4ec380985da0a7970d6491792f9c4fb8b1e
gekwealor <gekwealor@gmail.com>
2023-05-27 10:11:39 -0700

    Get precision

--- get_precision.c
+++ get_precision.c
@@ -13,7 +13,7 @@ int get_precision(const char *format, int *i, va_list list)
 	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[curr_i] !=  '.')
+	if (format[curr_i] != '.')
 		return (precision);
 
 	precision = 0;
@@ -23,13 +23,13 @@ int get_precision(const char *format, int *i, va_list list)
 		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
-			precision += format[curr_i] - '0';
+			precision += format[curr_i++)
 		}
 		else if (format[curr_i] == '*')
-		{	
-		curr_i++;
-		precision = va_arg(list, int);
-		break;
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
 		}
 		else
 			break;
